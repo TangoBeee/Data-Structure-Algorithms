5543eb6c2646f68203009b658ba12fd05701d165
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-09-29 17:21:47 +0530

    Create 26 | Remove Duplicates from Sorted Array.java

---
+++ Arrays/26 | Remove Duplicates from Sorted Array.java
@@ -0,0 +1,29 @@
+/* |----------------------------------------------------------------------------------------------|
+   |------------------------Naive/Optimal Solution (TC: O(N^2) | SC: O(1))--------------------------|
+   |----------------------------------------------------------------------------------------------| */
+
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        int n = nums.length;
+        
+        int left = 1;
+        int right = 1;
+        
+        while(right < n) {
+            if(nums[right-1] == nums[right]) {
+                right++;
+                continue;
+            }
+            
+            else if(nums[right-1] < nums[right]) {
+                nums[left] = nums[right];
+                right++;
+                left++;
+            }
+        }
+        
+        return left;
+    }
+}
