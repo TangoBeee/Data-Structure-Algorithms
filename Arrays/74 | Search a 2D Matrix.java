f0c029444a469849be3632bea8129141b556db85
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-04 00:16:45 +0530

    Update 74 | Search a 2D Matrix.java

--- Arrays/74 | Search a 2D Matrix.java
+++ Arrays/74 | Search a 2D Matrix.java
@@ -23,7 +23,7 @@ class Solution {
 }
 
 /* |----------------------------------------------------------------------------------------------|
-   |--------------------------Optimal Solution (TC: O(N) | SC: O(1))------------------------------|
+   |---------------------Optimal Solution (TC: O(N * (Log M)) | SC: O(1))-------------------------|
    |----------------------------------------------------------------------------------------------| */
 
 class Solution {
@@ -54,6 +54,33 @@ class Solution {
             else end = mid-1;
         }
     
+        return false;
+    }
+}
+
+
+/* |----------------------------------------------------------------------------------------------|
+   |-----------------------Optimal Solution (TC: O(Log(n m)) | SC: O(1))--------------------------|
+   |----------------------------------------------------------------------------------------------| */
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        
+        int start = 0;
+        int end = (n*m)-1;
+        
+        while(start <= end) {
+            int mid = (start + (end-start)/2);
+            
+            if(matrix[mid/m][mid%m] == target) return true;
+            
+            else if(matrix[mid/m][mid%m] < target) start = mid+1;
+            
+            else end = mid-1;
+        }
+        
         return false;
     }
 }
