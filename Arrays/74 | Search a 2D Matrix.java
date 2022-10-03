9ee5691515fb6d2fed6996e43b9825ec1bb933f7
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-03 20:38:48 +0530

    Update 74 | Search a 2D Matrix.java

--- Arrays/74 | Search a 2D Matrix.java
+++ Arrays/74 | Search a 2D Matrix.java
@@ -26,3 +26,34 @@ class Solution {
    |--------------------------Optimal Solution (TC: O(N) | SC: O(1))------------------------------|
    |----------------------------------------------------------------------------------------------| */
 
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        
+        boolean ans = false;
+        
+        for(int i = 0; i < n; i++) {
+            
+            ans = helper(matrix[i], 0, matrix[i].length-1, target);
+            if(ans == true) break;
+            
+        }
+        
+        return ans;
+    }
+    
+    private boolean helper(int[] nums, int start, int end, int target) {
+        
+        while(start <= end) {
+            int mid = (start+end)/2;
+            
+            if(nums[mid] == target) return true;
+            
+           else  if(nums[mid] < target) start = mid+1;
+            
+            else end = mid-1;
+        }
+    
+        return false;
+    }
+}
