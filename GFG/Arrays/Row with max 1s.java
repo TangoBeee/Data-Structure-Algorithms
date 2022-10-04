d07806860a651b7cea6648e49b387242819c2460
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-04 14:25:29 +0530

    Adding Optimal Solution

--- GFG/Arrays/Row with max 1s.java
+++ GFG/Arrays/Row with max 1s.java
@@ -26,4 +26,25 @@ class Solution {
   
   /*------------------------------------------------------------------------*/
   
-  
+class Solution {
+
+    int rowWithMax1s(int arr[][], int n, int m) {
+        int i=0, j = m-1, res = -1;
+        while(i<n&&j>=0){
+            if(arr[i][j]==1){
+        	    j--;
+        	    res = i;
+        	}
+        	else {
+        	    i++;
+        	}
+        }
+        return res;
+    }
+
+}
+    
+//TC: O(N+M)
+//SC: O(1)
+    
+   //Optimal Solution ^^
