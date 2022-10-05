1f599509f569d3fe9a250c307d3ab78edcffb798
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-05 21:41:21 +0530

    Optimizing the solution for new test cases

--- Arrays/53 | Maximum Subarray.java
+++ Arrays/53 | Maximum Subarray.java
@@ -26,7 +26,7 @@ class Solution {
 
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum = 0;
+        int sum = Integer.MIN_VALUE;
         
         for(int i = 0; i < nums.length; i++) {
             int temp = 0;
