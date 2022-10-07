dd1b298a32af513148ee4e005bef999edf5be1c4
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-07 22:15:11 +0530

    Adding Optimal Solution

--- Arrays/73 | Set Matrix Zeroes.java
+++ Arrays/73 | Set Matrix Zeroes.java
@@ -78,3 +78,48 @@ class Solution {
    |---------------------------Optimal Solution (TC: O(N*M) | SC: O(1))---------------------------|
    |----------------------------------------------------------------------------------------------| */
 
+class Solution {
+    public void setZeroes(int[][] matrix){
+
+		int m=matrix.length, n=matrix[0].length;
+        
+        boolean isRow0=false, isCol0=false;
+        
+        for(int i = 0; i < m; i++) {
+            if(matrix[i][0] == 0) isCol0 = true;
+        }
+        
+        for(int i = 0; i < n; i++) {
+            if(matrix[0][i] == 0) isRow0 = true;
+        }
+        
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+        }
+
+        
+        if(isRow0) {
+            for(int i = 0; i < n; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        
+        if(isCol0) {
+            for(int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
