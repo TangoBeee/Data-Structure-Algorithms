964cd1b73e9eae1cff57e4113606f4457af89081
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-10 20:43:33 +0530

    Adding Optimal Solution

--- Arrays/229 | Majority Element II.java
+++ Arrays/229 | Majority Element II.java
@@ -45,4 +45,49 @@ class Solution {
    |---------------------------Optimal Solution (TC: O(N) | SC: O(1))-----------------------------|
    |----------------------------------------------------------------------------------------------| */
 
-
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        
+        int num1 = Integer.MIN_VALUE;
+        int num2 = Integer.MIN_VALUE;
+        
+        int count1 = 0;
+        int count2 = 0;
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(num1 == nums[i]) count1++;
+            
+            else if(num2 == nums[i]) count2++;
+            
+            else if(count1 == 0) {
+                num1 = nums[i];
+                count1 = 1;
+            }
+            
+            else if(count2 == 0) {
+                num2 = nums[i];
+                count2 = 1;
+            }
+            
+            else {
+                count1--;
+                count2--;
+            }
+        }
+        
+        count1 = 0;
+        count2 = 0;
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(num1 == nums[i]) count1++;
+            else if(num2 == nums[i]) count2++;
+        }
+        
+        if(count1 > nums.length/3) list.add(num1);
+        if(count2 > nums.length/3) list.add(num2); 
+        
+        return list;
+    }
+}
