32546699f7fdbd33bf907e436a75f7292af1d7ae
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-10 16:53:08 +0530

    Create 118 | Pascal's Triangle.java

---
+++ Arrays/118 | Pascal's Triangle.java
@@ -0,0 +1,26 @@
+/* |----------------------------------------------------------------------------------------------|
+   |------------------------Optimal Solution (TC: O(N^2) | SC: O(N^2))----------------------------|
+   |----------------------------------------------------------------------------------------------| */
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> list = new ArrayList<>();
+        
+        List<Integer> row, prev = null;
+        
+        for(int i = 0; i < numRows; i++) {
+            row = new ArrayList<>();
+            for(int j = 0; j <= i; j++) {
+                if(j == 0 || j == i)
+                    row.add(1);
+                else
+                    row.add(prev.get(j-1) + prev.get(j));
+            }
+            
+            prev = row;
+            list.add(row);
+        }
+        
+        return list;
+    }
+}
