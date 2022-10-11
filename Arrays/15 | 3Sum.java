ae356fc1f08a0de8dcde4d7776c6b06c8d7f3c51
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-11 23:54:40 +0530

    Adding Optimal Solution

--- Arrays/15 | 3Sum.java
+++ Arrays/15 | 3Sum.java
@@ -29,6 +29,37 @@ class Solution {
 }
 
 /* |----------------------------------------------------------------------------------------------|
-   |-------------------------Better Solution (TC: O(N^2) | SC: O(M+N))----------------------------|
+   |-------------------------Optimal Solution (TC: O(N^2) | SC: O(N))-----------------------------|
    |----------------------------------------------------------------------------------------------| */
 
+class Solution {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        
+        Arrays.sort(nums);
+        
+        List<List<Integer>> result = new ArrayList<>();
+        
+        for(int i = 0; i < nums.length-2; i++) {
+            if(i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+                int low = i+1, high = nums.length-1, sum = 0 - nums[i];
+                while(low < high) {
+                    if(nums[low]+nums[high] == sum) {
+                        result.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                        
+                        while(low < high && nums[low] == nums[low+1]) low++;
+                        while(low < high && nums[high] == nums[high-1]) high--;
+                        
+                        low++; high--;
+                    }
+                    
+                    else {
+                        if(nums[low]+nums[high] < sum) low++;
+                        else high--;
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+}
