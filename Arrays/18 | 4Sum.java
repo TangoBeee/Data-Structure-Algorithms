815df2b6e94a98c9bed2b66897a3ade2dcd3db1e
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-13 00:33:39 +0530

    Update 18 | 4Sum.java

--- Arrays/18 | 4Sum.java
+++ Arrays/18 | 4Sum.java
@@ -1,5 +1,5 @@
 /* |----------------------------------------------------------------------------------------------|
-   |--------------------------Naive Solution (TC: O(N^4) | SC: O(M))------------------------------|
+   |--------------------------Naive Solution (TC: O(N^4) | SC: O(1))------------------------------|
    |----------------------------------------------------------------------------------------------| */
 
 class Solution {
@@ -29,3 +29,72 @@ class Solution {
         return result;
     }
 }
+
+// TC could be N^5 because of list.contains();
+
+/* |----------------------------------------------------------------------------------------------|
+   |--------------------------Naive Solution (TC: O(N^4) | SC: O(1))------------------------------|
+   |----------------------------------------------------------------------------------------------| */
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        Arrays.sort(nums);
+        
+        for(int i = 0; i < nums.length-3; i++) {
+            for(int j = i+1; j < nums.length; j++) {
+                for(int k = j+1; k < nums.length; k++) {
+                    int l = k+1;
+                    int sum = nums[i] + nums[j] + nums[k];
+                    while(l < nums.length) {
+                        if(sum + nums[l] == target) {
+                            if(!result.contains(Arrays.asList(nums[i], nums[j], nums[k], nums[l])))
+                                result.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                            
+                            while(l < nums.length && nums[l] == nums[i+1]) l++;
+                            l++;
+                        }
+                        else l++;
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+}
+
+// TC could be N^5 because of list.contains();
+
+
+/* |----------------------------------------------------------------------------------------------|
+   |--------------------------Better Solution (TC: O(N^3) | SC: O(1))------------------------------|
+   |----------------------------------------------------------------------------------------------| */
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        
+        List<List<Integer>> result = new ArrayList<>();
+        
+        Arrays.sort(nums);
+        
+        for(int i = 0; i < nums.length-3; i++) {
+            for(int j = i+1; j < nums.length; j++) {
+                int low = j+1, high = nums.length-1;
+                long sum = nums[i]+nums[j];
+                while(low < high) {
+                    long temp = nums[low]+nums[high];
+                    if(sum+temp == target && !result.contains(Arrays.asList(nums[i], nums[j], nums[low], nums[high])))
+                        result.add(Arrays.asList(nums[i], nums[j], nums[low++], nums[high--]));
+                    else if(sum+temp < target) low++;
+                    else high--;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
+
+// TC could be N^4 because of list.contains();
