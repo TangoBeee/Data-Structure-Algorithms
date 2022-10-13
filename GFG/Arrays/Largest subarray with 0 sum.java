0ccd4506cfe59fc7d3df6aa896f115fd77400de0
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-13 19:18:15 +0530

    adding optimal solution

--- GFG/Arrays/Largest subarray with 0 sum.java
+++ GFG/Arrays/Largest subarray with 0 sum.java
@@ -52,3 +52,36 @@ class GfG
 
 /* ---------------------------------------------------------------------------------- */
 
+class GfG
+{
+    int maxLen(int nums[], int n)
+    {
+		int max = 0;
+		int sum = 0;
+		
+		HashMap<Integer, Integer> map = new HashMap<>();
+
+		for(int i = 0; i < n; i++) {
+		    sum += nums[i];
+		    
+			if(sum == 0) {
+			    max = i+1;
+			}
+			
+			else {
+			    if(map.get(sum) != null) {
+			        max = Math.max(max, i-map.get(sum));
+			    }
+			    else {
+			        map.put(sum, i);
+			    }
+			}
+		}
+
+		return max;
+    }
+}
+
+// TC -> O(N)
+// SC -> O(N)
+// Optimal Solution
