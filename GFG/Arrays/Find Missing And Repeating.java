deeec0996e1f82597141590f9ff19beda44be7e1
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-19 22:48:35 +0530

    Update Find Missing And Repeating.java

--- GFG/Arrays/Find Missing And Repeating.java
+++ GFG/Arrays/Find Missing And Repeating.java
@@ -28,3 +28,27 @@ class Solve {
 ----------------------------------------------------------------------------------
   
   
+
+class Solve {
+    int[] findTwoElement(int nums[], int n) {
+		long miss = 0;
+		long dup = 0;
+
+		long sum = (n*(n+1))/2; //get sum from 1 to n (1 + 2 + .... + n)
+		long power = (n*(n+1) * (2*n+1))/6;//get squre sum from 1 to n (1^2 + 2^2 + .... + n^2)
+
+		for(int i = 0; i < n; i++) {
+			sum -= (long)nums[i];
+			power -= (long)(nums[i] * nums[i]);
+		}
+
+		miss = (sum + power/sum)/2;
+		dup = miss-sum;
+
+		return new int[] {(int)dup, (int)miss};
+    }
+}
+
+// TC: O(N)
+// SC: O(1)
+// Optimal Solution using Math trick
