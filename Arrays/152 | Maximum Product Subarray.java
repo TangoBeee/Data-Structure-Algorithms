aa8bc27b8b5360fdd91c8216b1dbfbe19a3ec938
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-20 23:31:20 +0530

    Adding Optimal Solution

--- Arrays/152 | Maximum Product Subarray.java
+++ Arrays/152 | Maximum Product Subarray.java
@@ -23,6 +23,34 @@ class Solution {
 
 
 /* |----------------------------------------------------------------------------------------------|
-   |--------------------------Optimal Solution (TC: O(N) | SC: O(N))------------------------------|
+   |--------------------------Optimal Solution (TC: O(N) | SC: O(1))------------------------------|
    |----------------------------------------------------------------------------------------------| */
    
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int prefixProd = 1;
+        
+        for(int i = 0; i < nums.length; i++) {
+            prefixProd *= nums[i];
+            
+            max = Math.max(max, prefixProd);
+            
+            if(prefixProd == 0) 
+                prefixProd = 1;
+        }
+        
+        prefixProd = 1;
+        
+        for(int i = nums.length-1; i >= 0; i--) {
+            prefixProd *= nums[i];
+            
+            max = Math.max(max, prefixProd);
+            
+            if(prefixProd == 0) 
+                prefixProd = 1;
+        }
+        
+        return max;
+    }
+}
