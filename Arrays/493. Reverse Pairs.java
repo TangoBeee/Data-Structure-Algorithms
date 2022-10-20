ba2b14be494631217c81de391f467bc80fdb9383
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-20 17:15:59 +0530

    Adding Optimal Solution

--- Arrays/493. Reverse Pairs.java
+++ Arrays/493. Reverse Pairs.java
@@ -20,5 +20,71 @@ class Solution {
 
 
 /* |----------------------------------------------------------------------------------------------|
-   |---------------------------Optimal Solution (TC: O() | SC: O())-------------------------------|
+   |------------------------Optimal Solution (TC: O(N logN) | SC: O(N))---------------------------|
    |----------------------------------------------------------------------------------------------| */
+
+class Solution {
+    public int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length-1);
+    }
+    
+    private int mergeSort(int[] nums, int left, int right) {
+        if(left >= right) return 0;
+        
+        int count = 0;
+        
+        int mid = (left+right)/2;
+        
+        count += mergeSort(nums, left, mid);
+        count += mergeSort(nums, mid+1, right);
+        
+        count += merge(nums, left, mid, right);
+        
+        return count;
+    }
+    
+    private int merge(int[] nums, int left, int mid, int right) {
+        int count = 0, k = 0;
+        
+        int[] temp = new int[right-left+1];
+        
+        int i = left;
+        int j = mid+1;
+        
+        while(i <= mid && j <= right) {
+            if((long)nums[i] > (long)2*nums[j]) {
+                count += mid+1-i;
+                j++;
+            }
+            else {
+                i++;
+            }
+                
+        }
+                
+        i = left;
+        j = mid+1;
+        
+        while(i <= mid && j <= right) {
+            if(nums[i] <= nums[j])
+                temp[k] = nums[i++];
+            else {
+                temp[k] = nums[j++];
+            }
+            k++;
+        }
+        
+        while(i <= mid)
+            temp[k++] = nums[i++];
+        while(j <= right)
+            temp[k++] = nums[j++];
+        
+        for(int indext = 0, indexo = left; indext < temp.length; indext++) {
+            nums[indexo++] = temp[indext];
+        }
+        
+        return count;
+    }
+}
+
+
