eb671c5750df09205b3934a3f6247bd3f6d1d003
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-21 12:01:32 +0530

    Update Largest Element in Array.java

--- GFG/Arrays/Largest Element in Array.java
+++ GFG/Arrays/Largest Element in Array.java
@@ -2,6 +2,23 @@ class Compute {
     
     public int largest(int arr[], int n)
     {
+        Arrays.sort(arr);
+        
+        return arr[n-1];
+    }
+}
+
+// TC : O(N logN)
+// SC : O(1)
+
+//Naive Solution
+
+/* --------------------------------------------------------------------- */
+
+class Compute {
+    
+    public int largest(int arr[], int n)
+    {
         int ans = 0;
         
         for(int i = 0; i < n; i++) {
@@ -14,3 +31,5 @@ class Compute {
 
 // TC: O[n] 
 // SC: O[1]
+
+//Optimal Solution
