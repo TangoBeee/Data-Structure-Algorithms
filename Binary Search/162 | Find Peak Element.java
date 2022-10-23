6be074343487cdb12030d3cfefb4cd13500cb0c2
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-24 00:58:20 +0530

    Adding Optimal Solution

--- Binary Search/162 | Find Peak Element.java
+++ Binary Search/162 | Find Peak Element.java
@@ -25,4 +25,27 @@ class Solution {
    |------------------------Optimal Solution (TC: O(log N) | SC: O(1))----------------------------|
    |----------------------------------------------------------------------------------------------| */
 
-
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if(nums.length < 2)
+            return nums.length-1;
+        
+        if(nums[0] > nums[1])
+            return 0;
+        
+        int i = 1, j = nums.length-2;
+        
+        while(i <= j) {
+            int mid = i+(j-i)/2;
+            
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1])
+                return mid;
+            else if(nums[mid] < nums[mid+1])
+                i = mid+1;
+            else
+                j = mid-1;
+        }
+        
+        return nums.length-1;
+    }
+}
