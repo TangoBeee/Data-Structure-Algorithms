a436e1d2b4f4f229684cb4918486f11c4ee71c40
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-25 22:52:02 +0530

    Update 153 | Find Minimum in Rotated Sorted Array.java

--- Binary Search/153 | Find Minimum in Rotated Sorted Array.java
+++ Binary Search/153 | Find Minimum in Rotated Sorted Array.java
@@ -20,4 +20,63 @@ class Solution {
    |------------------------Optimal Solution (TC: O(log N) | SC: O(1))----------------------------|
    |----------------------------------------------------------------------------------------------| */
 
+class Solution {
+    public int findMin(int[] nums) {
+        
+        if (nums.length == 1) 
+            return nums[0];
+        
+        int i = 0, j = nums.length - 1;
+
+        if (nums[j] > nums[0]) 
+            return nums[0];
+
+        while (j >= i) {
+            
+            int mid = i + (j - i) / 2;
+
+            if (nums[mid] > nums[mid + 1])
+                return nums[mid + 1];
+
+            if (nums[mid - 1] > nums[mid]) 
+                return nums[mid];
+
+            if (nums[mid] > nums[0]) 
+                i = mid + 1;
+            
+            else 
+                j = mid - 1;
+        }
+        
+        
+        return -1;
+    }
+}
+
+
+/* |----------------------------------------------------------------------------------------------|
+   |------------------------Optimal Solution (TC: O(log N) | SC: O(1))----------------------------|
+   |----------------------------------------------------------------------------------------------| */
+
+//Less operations as compared to above solution
+
+class Solution {
+    public int findMin(int[] nums) {
+        
+        int i= 0, j = nums.length-1;
+        
+        while(i < j) {
+            
+            int mid = i + (j-i)/2;
+            
+            if(nums[mid] > nums[j])
+                i = mid + 1;
+            
+            else
+                j = mid;
+        }
+        
+        return nums[i];
+    }
+}
 
