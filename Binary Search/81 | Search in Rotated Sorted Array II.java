a66d7a25e6aab0812dd1b48836af2498cc39259a
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-25 14:03:35 +0530

    Update 81 | Search in Rotated Sorted Array II.java

--- Binary Search/81 | Search in Rotated Sorted Array II.java
+++ Binary Search/81 | Search in Rotated Sorted Array II.java
@@ -19,5 +19,34 @@ class Solution {
    |------------------------Optimal Solution (TC: O(log N) | SC: O(1))----------------------------|
    |----------------------------------------------------------------------------------------------| */
    
-   
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int i = 0, j = nums.length-1;
+        
+        while(i <= j) {
+            int mid = (i+j)>>1;
+            
+            if(nums[mid] == target)
+                return true;
+            else if(nums[i] < nums[mid] || nums[mid] > nums[j]) {
+                if(target >= nums[i] && target <= nums[mid])
+                    j = mid-1;
+                else
+                    i = mid+1;
+            }
+            
+            else if(nums[mid] < nums[j] || nums[mid] < nums[i]) {
+                if(target >= nums[mid] && target <= nums[j])
+                    i = mid+1;
+                else
+                    j = mid-1;
+            }
+            
+            else
+                j--;
+        }
+        
+        return false;
+    }
+}   
    
