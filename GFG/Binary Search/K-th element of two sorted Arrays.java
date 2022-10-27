fa02a8e0c54ff1c854b83fb6a8d4ba589da9641d
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-28 01:15:37 +0530

    Update K-th element of two sorted Arrays.java

--- GFG/Binary Search/K-th element of two sorted Arrays.java
+++ GFG/Binary Search/K-th element of two sorted Arrays.java
@@ -1,1 +1,78 @@
 
+class Solution {
+    public long kthElement( int arr1[], int arr2[], int n, int m, int k) {
+        int[] nums = new int[n+m];
+
+        int i = 0, j = 0;
+        int t = 0;
+
+        while(i < n && j < m) {
+        	if(arr1[i] <= arr2[j])
+        		nums[t] = arr1[i++];
+        	else if(arr1[i] >= arr2[j])
+        		nums[t] = arr2[j++];
+        	t++;
+        }
+
+        while(i < n)
+        	nums[t++] = arr1[i++];
+
+        while(j < m)
+        	nums[t++] = arr2[j++];
+
+
+        return (long)nums[k-1];
+        
+    }
+}
+
+//TC: O(N+M)
+//SC: O(N+M)
+
+// Naive Solution
+
+/* -------------------------------------------------------------------------------------------------- */
+
+class Solution {
+    public long kthElement( int arr1[], int arr2[], int n, int m, int k) {
+
+        int i = 0, j = 0;
+        int t = 0, ans = 0;
+
+        while(i < n && j < m) {
+        	if(t == k) break;
+
+        	else if(arr1[i] <= arr2[j])
+        		ans = arr1[i++];
+        	else if(arr1[i] >= arr2[j])
+        		ans = arr2[j++];
+
+        	t++;
+        }
+
+        while(i < n) {
+        	if(t == k) break;
+        	ans = arr1[i++];
+        	t++;
+        }
+
+        while(j < m) {
+        	if(t == k) break;
+        	ans = arr2[j++];
+        	t++;
+        }
+
+
+        return (long)ans;
+        
+    }
+}
+
+//TC: O(K)
+//SC: O(1)
+
+// Better Solution ^^
+
+/* ----------------------------------------------------------------------------------------------- */
+
+
