c95d39b98aeb0a2ef0967ee811ba93d1c726aff2
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-30 23:27:10 +0530

    Update K-th element of two sorted Arrays.java

--- GFG/Binary Search/K-th element of two sorted Arrays.java
+++ GFG/Binary Search/K-th element of two sorted Arrays.java
@@ -75,7 +75,37 @@ class Solution {
 
 /* ----------------------------------------------------------------------------------------------- */
 
-
+class Solution {
+    public long kthElement( int arr1[], int arr2[], int n, int m, int k) {
+        
+        if(n > m)
+            kthElement(arr2, arr1, m, n, k);
+        
+        int low = Math.max(0,k-m), high = Math.min(k,n);
+        
+        while(low <= high) {
+            int cut1 = (low+high)/2;
+            int cut2 = k-cut1;
+            
+            long left1 = cut1 == 0 ? Integer.MIN_VALUE : arr1[cut1-1];
+            long left2 = cut2 == 0 ? Integer.MIN_VALUE : arr2[cut2-1];
+            
+            long right1 = cut1 == n ? Integer.MAX_VALUE : arr1[cut1];
+            long right2 = cut2 == m ? Integer.MAX_VALUE : arr2[cut2];
+            
+            if(left1 <= right2 && left2 <= right1)
+                return Math.max(left1, left2);
+            
+            else if(left1 > right2)
+                high = cut1-1;
+                
+            else
+                low = cut1+1;
+        }
+        
+        return -1;
+    }
+}
 
 //TC: O(Log(min(n m)))
 //sc: O(1)
