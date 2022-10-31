e8ba65988a8768d7a6ee7729ea593b277d5c1b67
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-10-31 16:29:16 +0530

    Update 74 | Search a 2D Matrix.java

--- Binary Search/74 | Search a 2D Matrix.java
+++ Binary Search/74 | Search a 2D Matrix.java
@@ -16,6 +16,40 @@ class Solution {
 
 
 /* |----------------------------------------------------------------------------------------------|
+   |--------------------------Better Solution (TC: O(N+M) | SC: O(1))-----------------------------|
+   |----------------------------------------------------------------------------------------------| */
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for(int[] i : matrix)
+            if(binarySearch(i, target))
+                return true;
+        
+        return false;
+    }
+    
+    private boolean binarySearch(int[] nums, int target) {
+        int low = 0, high = nums.length-1;
+        
+        while(low <= high) {
+            int mid = low+(high-low)/2;
+            
+            if(nums[mid] == target)
+                return true;
+            
+            else if(nums[mid] < target)
+                low = mid+1;
+            
+            else
+                high = mid-1;
+        }
+        
+        return false;
+    }
+}
+
+
+/* |----------------------------------------------------------------------------------------------|
    |-----------------------Optimal Solution (TC: O(log(N M)) | SC: O(1))--------------------------|
    |----------------------------------------------------------------------------------------------| */
 
