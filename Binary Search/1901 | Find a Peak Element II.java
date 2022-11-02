f1e9ad363fee7f1c73203a6a37ebed56f97d8cb2
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-11-02 19:52:00 +0530

    Update 1901 | Find a Peak Element II.java

--- Binary Search/1901 | Find a Peak Element II.java
+++ Binary Search/1901 | Find a Peak Element II.java
@@ -60,4 +60,71 @@ class Solution {
    |-----------------------Optimal Solution (TC: O(N log(m)) | SC: O(1))--------------------------|
    |----------------------------------------------------------------------------------------------| */
 
+class Solution {
+    public int[] findPeakGrid(int[][] mat) {
+        int low = 0, high = mat.length-1;
+        
+        while(low <= high) {
+            int mid = (low+high) >>> 1;
+            
+            int maxRow = maxRowElement(mat[mid], mat[mid].length);
+            
+            if(mid == 0)
+                if(mat[mid][maxRow] > mat[mid+1][maxRow])
+                    return new int[] {mid, maxRow};
+            if(mid == mat.length-1)
+                if(mat[mid][maxRow] > mat[mid-1][maxRow])
+                    return new int[] {mid, maxRow};
+            
+            if(mat[mid][maxRow] < mat[mid+1][maxRow])
+                low = mid+1;
+            else if(mat[mid][maxRow] < mat[mid-1][maxRow])
+                high = mid-1;
+            
+            else
+                return new int[] {mid, maxRow};
+        }
+        
+        return null;
+    }
+    
+    private int maxRowElement(int[] nums, int n) {
+        int max = 0;
+        
+        for(int i = 0; i < n; i++) {
+            if(nums[i] > nums[max])
+                max = i;
+        }
+        
+        return max;
+    }
+}
 
+// ------------------------------------ OR ---------------------------------- //
+
+class Solution {
+    public int[] findPeakGrid(int[][] mat) {
+        int startCol = 0, endCol = mat[0].length-1;
+        
+        while(startCol <= endCol){
+            int maxRow = 0, midCol = startCol + (endCol-startCol)/2;
+            
+            for(int row=0; row<mat.length; row++){
+                 maxRow = mat[row][midCol] >= mat[maxRow][midCol]? row : maxRow;  
+            }
+            
+            boolean leftIsBig    =   midCol-1 >= startCol  &&  mat[maxRow][midCol-1] > mat[maxRow][midCol];
+            boolean rightIsBig   =   midCol+1 <= endCol    &&  mat[maxRow][midCol+1] > mat[maxRow][midCol];
+            
+            if(!leftIsBig && !rightIsBig)   // we have found the peak element
+                return new int[]{maxRow, midCol};
+            
+            else if(rightIsBig)  // if rightIsBig, then there is an element in 'right' that is bigger than all the elements in the 'midCol', 
+                startCol = midCol+1; //so 'midCol' cannot have a 'peakPlane'
+            
+            else // leftIsBig
+                endCol = midCol-1;
+        }
+        return null;
+    }
+}
