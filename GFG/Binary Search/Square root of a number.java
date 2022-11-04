bf9aba81c9ca0c67a349bb63e23443f2c14881f2
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-11-05 01:02:08 +0530

    Create Square root of a number.java

---
+++ GFG/Binary Search/Square root of a number.java
@@ -0,0 +1,17 @@
+class Solution
+{
+     long floorSqrt(long x)
+	 {
+		// Your code here
+		return (long)Math.sqrt(x);
+	 }
+}
+
+//TC: O(log x) <i'm not sure>
+//SC: O(1)
+
+//Naive Solution ^^
+
+/* ------------------------------------------------------------------------------ */
+
+
