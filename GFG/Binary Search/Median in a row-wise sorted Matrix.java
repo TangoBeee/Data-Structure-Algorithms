bf5cba5cba46fd872357d753ab164b9e0246adb8
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-11-05 00:25:51 +0530

    Update Median in a row-wise sorted Matrix.java

--- GFG/Binary Search/Median in a row-wise sorted Matrix.java
+++ GFG/Binary Search/Median in a row-wise sorted Matrix.java
@@ -22,3 +22,68 @@ class Solution {
 /* --------------------------------------------------------------------------- */
 
 
+class Solution {
+    int median(int matrix[][], int R, int C) {
+        int low = minFinder(matrix);
+        int high = maxFinder(matrix);
+        
+        int minNumbers = ((R*C)/2)+1;
+        
+        while(low <= high) {
+            int mid = (low+high) >>> 1;
+            int numCount = numberOfElements(matrix, mid);
+            
+            if(numCount >= minNumbers)
+                high = mid-1;
+            else
+                low = mid+1;
+        }
+        
+        return low;
+    }
+    
+    //Finding how many numbers are smaller or equal(because in matrix we have duplicate numbers) then target.
+    private int numberOfElements(int[][] mat, int target) {
+        int count = 0;
+        
+        for(int[] i : mat) {
+            for(int j : i) {
+                if(j <= target)
+                    count++;
+            }
+        }
+        
+        return count;
+    }
+    
+    //Finding smallest element in matrix
+    private int minFinder(int[][] mat) {
+        int min = Integer.MAX_VALUE;
+        
+        for(int[] i : mat) {
+            for(int j : i) {
+                min = j <= min ? j : min;
+            }
+        }
+        
+        return min;
+    }
+    
+    //Finding largest element in the matrix
+    private int maxFinder(int[][] mat) {
+        int max = Integer.MIN_VALUE;
+        
+        for(int[] i : mat) {
+            for(int j : i) {
+                max = j >= max ? j : max;
+            }
+        }
+        
+        return max;
+    }
+}
+
+//TC: O(32 * R * log(C))
+//SC: O(1)
+
+//Optimal Solution ^^
