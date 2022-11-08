73bc483e3b4e262428d3f0a8d7653bb1a3244276
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-11-08 23:44:10 +0530

    Update 1283 | Find the Smallest Divisor Given a Threshold.java

--- Binary Search/1283 | Find the Smallest Divisor Given a Threshold.java
+++ Binary Search/1283 | Find the Smallest Divisor Given a Threshold.java
@@ -1,6 +1,6 @@
 /* |----------------------------------------------------------------------------------------------|
    |-------------------------Naive Solution (TC: O(N*MAX) | SC: O(1))-----------------------------|
-   |----------------------------------------------------------------------------------------------| */
+   |----------------------------------------------------------------------------------------------| */ //Where MAX is maximum element of the array
 
 class Solution {
     public int smallestDivisor(int[] nums, int threshold) {
@@ -31,7 +31,34 @@ class Solution {
 
 
 /* |----------------------------------------------------------------------------------------------|
-   |--------------------------Optimal Solution (TC: O() | SC: O(1))-------------------------------|
-   |----------------------------------------------------------------------------------------------| */
+   |---------------------Optimal Solution (TC: O(N*log(MAX)) | SC: O(1))--------------------------|
+   |----------------------------------------------------------------------------------------------| */ //Where MAX is maximum element of the array
 
+class Solution {
+    public int smallestDivisor(int[] nums, int threshold) {
+        int low = 1, high = Integer.MIN_VALUE;
+
+        for(int i : nums) high = Math.max(high, i);
+        int ans = 0;
+
+        while(low <= high) {
+            int mid = (low+high) >>> 1;
+
+            int sum = 0;
+
+            for(int i : nums)
+                sum += (int)Math.ceil((double)i/(double)mid);
+            
+            if(sum <= threshold) {
+                ans = mid;
+                high = mid-1;
+            }
+
+            else
+                low = mid+1;
+        }
+
+        return ans;
+    }
+}
 
