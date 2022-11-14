9bfb0d7e73b76d34368bcc91fa0b1516fe9f2176
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-11-14 14:31:18 +0530

    Update 4 | Median of Two Sorted Arrays.java

--- Binary Search/4 | Median of Two Sorted Arrays.java
+++ Binary Search/4 | Median of Two Sorted Arrays.java
@@ -133,7 +133,45 @@ class Solution {
 
 
 /* |----------------------------------------------------------------------------------------------|
-   |-----------------------Optimal Solution (TC: O(log()) | SC: O(1))----------------------------|
+   |----------------------Optimal Solution (TC: O(log(N+M)) | SC: O(1))---------------------------|
    |----------------------------------------------------------------------------------------------| */
 
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 
+        if(nums2.length < nums1.length)
+            return findMedianSortedArrays(nums2, nums1);
+
+        int low = 0, high = nums1.length;
+        int len = ((nums1.length+nums2.length)+1)/2;
+
+        while(low <= high) {
+            int cut1 = (low+high) >>> 1;
+            int cut2 = len-cut1;
+
+            int left1 = cut1 <= 0 ? Integer.MIN_VALUE : nums1[cut1-1];
+            int left2 = cut2 <= 0 ? Integer.MIN_VALUE : nums2[cut2-1];
+
+            int right1 = cut1 >= nums1.length ? Integer.MAX_VALUE : nums1[cut1];
+            int right2 = cut2 >= nums2.length ? Integer.MAX_VALUE : nums2[cut2];
+
+
+            if(left1 <= right2 && left2 <= right1) {
+                if((nums1.length+nums2.length)%2 == 0)
+                    return (Math.max(left1, left2) + Math.min(right1, right2))/2.0;
+                else
+                    return Math.max(left1, left2);
+            }
+
+            else if(left1 > right2) {
+                high = cut1-1;
+            }
+
+            else {
+                low = cut1+1;
+            }
+        }
+
+        return -1;
+    }
+}
