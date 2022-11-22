b78a73301dbfcdb955a7bf4c05c3a1280d5a3fca
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-11-22 13:13:39 +0530

    Update 1539 | Kth Missing Positive Number.java

--- Binary Search/1539 | Kth Missing Positive Number.java
+++ Binary Search/1539 | Kth Missing Positive Number.java
@@ -30,3 +30,57 @@ class Solution {
 
     }
 }
+
+
+/* |----------------------------------------------------------------------------------------------|
+   |-------------------------Better Solution (TC: O(N) | SC: O(1))--------------------------------|
+   |----------------------------------------------------------------------------------------------| */
+
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+       
+       int arridx = 0, cnt = 0, ans = 2000;
+       
+       for(int i = 1;i<2000;i++) {
+           if(i == arr[arridx]) {
+               if(arridx != arr.length-1) arridx++;
+           }
+              
+           else cnt++;
+              
+           if(cnt == k){
+               ans = i;
+               break;
+           }
+       }
+            
+       return ans;
+    }
+}
+
+
+
+/* |----------------------------------------------------------------------------------------------|
+   |-----------------------Optimal Solution (TC: O(log(N)) | SC: O(1))----------------------------|
+   |----------------------------------------------------------------------------------------------| */
+
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+
+        int low = 0, high = arr.length-1;
+
+        while(low <= high) {
+            int mid = (low+high) >>> 1;
+
+            if(arr[mid]-(mid+1) > k) {
+                high = mid-1;
+            }
+
+            else {
+                low = mid+1;
+            }
+        }
+
+        return low+k;
+    }
+}
